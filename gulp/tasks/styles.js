911f9ebf4ba41a426f03e5993ea700f10a58c7ea
Patrick <info@patrickoutler.io>
2016-09-13 14:37:55 -0400

    Styling for hero and our beginning sections

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,10 +3,15 @@ var 	gulp = require('gulp'),
 		cssImport = require('postcss-import'),
 		nested = require('postcss-nested'),
 		cssVars = require('postcss-simple-vars'),
-		autoprefixer = require('autoprefixer');
+		autoprefixer = require('autoprefixer'),
+		mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe( postcss( [cssImport, cssVars, nested, autoprefixer] ) )
+		.pipe( postcss( [cssImport, mixins, cssVars, nested, autoprefixer] ) )
+		.on('error', function(errorInfo) {
+			console.log(errorInfo.toString());
+			this.emit('end');
+		})
 		.pipe( gulp.dest('./app/temp/styles') );
 });
